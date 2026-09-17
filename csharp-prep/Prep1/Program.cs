0a507ee24c24e512db86fe5b46f7857e17ba5d7c
Aaron-G-Riddle <rid20006@byui.edu>
2026-09-17 10:01:08 -0600

    Begun Prep 1, asking for first and last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.Write("Please enter first your name: ");
+        firstName = Console.ReadLine();
+        Console.Write("Please enter last your name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"{firstName} {lastName}");
+        
     }
 }
